3e51025c12f0bdcf3878c0d63882b5516ee59cdd
t yy <649909457@qq.com>
2020-01-13 11:26:20 +0800

    接口调整

--- models/platform/Wechat.php
+++ models/platform/Wechat.php
@@ -17,20 +17,31 @@ class Wechat extends BaseObject implements PayInterface
     public $data;
     public $error;
 
-    public function config()
+    public static function config($user_id)
     {
-        $c = Payment::find()->where(['code'=> Payment::PAY_WECHAT, 'user_id' => $this->data['owner_id']])->one();
+        $c = Payment::find()->where(['code'=> Payment::PAY_WECHAT, 'user_id' => $user_id])->one();
         if (!$c) {
             Tools::exceptionBreak(950002);
         }
-        $setting = Setting::getSetting($this->data['owner_id'], 'wx_');
+        $setting = Setting::getSetting($user_id, 'wx_');
         $cfg = json_decode($c->config,true);
         $app_key_pem = File::findOne($cfg['app_key_pem']);
         $app_cert_pem = File::findOne($cfg['app_cert_pem']);
         $cfg['app_key_pem'] = (Config::instance()->file_base_path ? Config::instance()->file_base_path : Yii::$app->basePath) . '/web' . $app_key_pem->file_url;
         $cfg['app_cert_pem'] = (Config::instance()->file_base_path ? Config::instance()->file_base_path : Yii::$app->basePath) . '/web' . $app_cert_pem->file_url;
         $cfg = array_merge($cfg, $setting);
-        return $cfg;
+        return [
+            'appid'          => $cfg['wx_applet_appid'],
+            'appsecret'      => $cfg['wx_applet_secret'],
+            'encodingaeskey' => '',
+            'mch_id'         => $cfg['mch_id'],
+            'mch_key'        => $cfg['md5_key'],
+            // 配置商户支付双向证书目录（可选，在使用退款|打款|红包时需要）
+            'ssl_key'        => $cfg['app_key_pem'],
+            'ssl_cer'        => $cfg['app_cert_pem'],
+            // 缓存目录配置（可选，需拥有读写权限）
+            'cache_path'     => '',
+        ];
     }
 
     public function app(){
@@ -43,40 +54,29 @@ class Wechat extends BaseObject implements PayInterface
     // public function pub(){};
 
     public function lite(){
-        $wx = UserWx::find()->select(['user_id', 'openid'])->where([
+        $map = [
                 'user_id'=> $this->data['user_id'], 
                 'owner_id'=> $this->data['owner_id']
-            ])->one();
+            ];
+        $wx = UserWx::find()->select(['user_id', 'openid'])->where($map)->one();
         if (empty($wx)) {
             Tools::exceptionBreak(950003);
         }
 
-        $config = $this->config();
         $pconf = Payment::configs(Payment::PAY_WECHAT);
         $notify_url = $pconf['const_config']['notify_url'];
-        $config = [
-            'appid'          => $config['wx_applet_appid'],
-            'appsecret'      => $config['wx_applet_secret'],
-            'encodingaeskey' => '',
-            'mch_id'         => $config['mch_id'],
-            'mch_key'        => $config['md5_key'],
-            // 配置商户支付双向证书目录（可选，在使用退款|打款|红包时需要）
-            'ssl_key'        => $config['app_key_pem'],
-            'ssl_cer'        => $config['app_cert_pem'],
-            // 缓存目录配置（可选，需拥有读写权限）
-            'cache_path'     => '',
-        ];
-        $wechat = new Pay($config);
-  
+        
+        $wechat = new Pay(self::config($this->data['owner_id']));
         // 组装参数，可以参考官方商户文档
         $options = [
-          'body'             => '支付单' . $this->data['payment_id'],
-          'out_trade_no'     => $this->data['payment_id'],
-          'total_fee'        => $this->data['money'] * 100,
-          'openid'           => $wx->openid,
-          'trade_type'       => 'JSAPI',
-          'notify_url'       => $notify_url,
-          'spbill_create_ip' => $this->data['ip'],
+            'body'             => '支付单' . $this->data['payment_id'],
+            'out_trade_no'     => $this->data['payment_id'],
+            'total_fee'        => $this->data['money'] * 100,
+            'openid'           => $wx->openid,
+            'trade_type'       => 'JSAPI',
+            'notify_url'       => $notify_url,
+            'spbill_create_ip' => $this->data['ip'],
+            'attach'           => base64_encode(json_encode($map))
         ];
 
         try {
